a33bba59790336d834b848e7ff0866f39a09ec81
Nathan Ribeiro de Araujo Oliveira <nathan.oliveira@sptech.school>
2023-05-31 20:45:54 -0300

    Cadastro e login ok

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -12,7 +12,7 @@ function listar() {
 function entrar(email, senha, token) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha, token)
     var instrucao = `
-    SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}'AND fkEmpresa = '${token}';
+    SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}'AND token = '${token}';
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -25,7 +25,7 @@ function cadastrar(nome, email, senha, cpf, token) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (nome, email, senha, cpf, fkEmpresa) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', '${token}');
+        INSERT INTO usuario (nome, email, senha, cpf, token) VALUES ('${nome}', '${email}', '${senha}', '${cpf}', '${token}');
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
