a5d54f71375c46b3b374e8caf6fbb43f6c4ba303
MariaPaulaBS <maria.bsilva@sptech.school>
2023-06-02 21:17:30 -0300

    Alertas e o resto dos gráficos estão feitos

--- site/public/dashboard/alerta.js
+++ site/public/dashboard/alerta.js
@@ -1,41 +1,38 @@
+token = sessionStorage.TOKEN_USUARIO;
 var alertas = [];
 
-function obterdados(idAquario) {
-    fetch(`/medidas/tempo-real/${idAquario}`)
-        .then(resposta => {
-
-            if (resposta.ok) {
-                resposta.json().then(resposta => {
-
-                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
-
-                    alertar(resposta, idAquario);
-                });
-            } else {
-
-                console.error('Nenhum dado encontrado ou erro na API');
-            }
-        })
-        .catch(function (error) {
-            console.error(`Erro na obtenção dos dados do aquario p/ gráfico: ${error.message}`);
-        });
+  
+function obterdados2(token){
+fetch(`/medidas/ultimas/${token}`)
+.then(resposta => {
+    if (resposta.ok) {
+        resposta.json().then(resposta =>{
+            console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+            alertar(resposta, token);
+        }); 
+    } else {
+      console.error('Nenhum dado encontrado ou erro na API');
+    }
+  })
+    .catch(function (error) {
+      console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+    });
 
 }
 
-function alertar(resposta, idAquario) {
+function alertar(resposta, token) {
     var temp = resposta[0].temperatura;
 
-    console.log(idAquario === resposta[0].fk_aquario)
+    console.log(token === resposta[0].fkSensor)
     
     var grauDeAviso ='';
 
-
     var limites = {
-        muito_quente: 23,
-        quente: 22,
-        ideal: 20,
-        frio: 10,
-        muito_frio: 5
+        muito_quente: 1,
+        quente: 0.6,
+        ideal: -1.05,
+        frio: -16,
+        muito_frio: -18.7
     };
 
     var classe_temperatura = 'cor-alerta';
@@ -44,57 +41,62 @@ function alertar(resposta, idAquario) {
         classe_temperatura = 'cor-alerta perigo-quente';
         grauDeAviso = 'perigo quente'
         grauDeAvisoCor = 'cor-alerta perigo-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+        exibirAlerta(temp, token, grauDeAviso, grauDeAvisoCor)
     }
     else if (temp < limites.muito_quente && temp >= limites.quente) {
         classe_temperatura = 'cor-alerta alerta-quente';
         grauDeAviso = 'alerta quente'
         grauDeAvisoCor = 'cor-alerta alerta-quente'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+        exibirAlerta(temp, token, grauDeAviso, grauDeAvisoCor)
     }
     else if (temp < limites.quente && temp > limites.frio) {
         classe_temperatura = 'cor-alerta ideal';
-        removerAlerta(idAquario);
+        removerAlerta(token);
     }
     else if (temp <= limites.frio && temp > limites.muito_frio) {
         classe_temperatura = 'cor-alerta alerta-frio';
         grauDeAviso = 'alerta frio'
         grauDeAvisoCor = 'cor-alerta alerta-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+        exibirAlerta(temp, token, grauDeAviso, grauDeAvisoCor)
     }
     else if (temp <= limites.muito_frio) {
         classe_temperatura = 'cor-alerta perigo-frio';
         grauDeAviso = 'perigo frio'
         grauDeAvisoCor = 'cor-alerta perigo-frio'
-        exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor)
+        exibirAlerta(temp, token, grauDeAviso, grauDeAvisoCor)
     }
 
-    var card;
+    // var card;
 
-    if (idAquario == 1) {
-        temp_aquario_1.innerHTML = temp + "°C";
-        card = card_1
-    } else if (idAquario == 2) {
-        temp_aquario_2.innerHTML = temp + "°C";
-        card = card_2
-    } else if (idAquario == 3) {
-        temp_aquario_3.innerHTML = temp + "°C";
-        card = card_3
-    } else if (idAquario == 4) {
-        temp_aquario_4.innerHTML = temp + "°C";
-        card = card_4
-    }
+    card_temp.innerHTML = temp + "°C";
+    
+    // card = document.getElementById("");
+    // card.className = classe_temperatura;
 
-    card.className = classe_temperatura;
+    // if (token == 1) {
+    //     temp_aquario_1.innerHTML = temp + "°C";
+    //     card = card_1
+    // } else if (token == 2) {
+    //     temp_aquario_2.innerHTML = temp + "°C";
+    //     card = card_2
+    // } else if (token == 3) {
+    //     temp_aquario_3.innerHTML = temp + "°C";
+    //     card = card_3
+    // } else if (token == 4) {
+    //     temp_aquario_4.innerHTML = temp + "°C";
+    //     card = card_4
+    // }
+
+
 }
 
-function exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor) {
-    var indice = alertas.findIndex(item => item.idAquario == idAquario);
+function exibirAlerta(temp, token, grauDeAviso, grauDeAvisoCor) {
+    var indice = alertas.findIndex(item => item.token == token);
 
     if (indice >= 0) {
-        alertas[indice] = { idAquario, temp, grauDeAviso, grauDeAvisoCor }
+        alertas[indice] = { token, temp, grauDeAviso, grauDeAvisoCor }
     } else {
-        alertas.push({ idAquario, temp, grauDeAviso, grauDeAvisoCor });
+        alertas.push({ token, temp, grauDeAviso, grauDeAvisoCor });
     }
 
     exibirCards();
@@ -103,8 +105,8 @@ function exibirAlerta(temp, idAquario, grauDeAviso, grauDeAvisoCor) {
 // que pode ser inserido clicando com o seu teclado em alt+255 ou pelo código adicionado acima.
 }
 
-function removerAlerta(idAquario) {
-    alertas = alertas.filter(item => item.idAquario != idAquario);
+function removerAlerta(token) {
+    alertas = alertas.filter(item => item.token != token);
     exibirCards();
 }
  
@@ -117,12 +119,151 @@ function exibirCards() {
     }
 }
 
-function transformarEmDiv({ idAquario, temp, grauDeAviso, grauDeAvisoCor }) {
+function transformarEmDiv({ token, temp, grauDeAviso, grauDeAvisoCor }) {
     return `<div class="mensagem-alarme">
     <div class="informacao">
     <div class="${grauDeAvisoCor}">&#12644;</div> 
-     <h3>Aquário ${idAquario} está em estado de ${grauDeAviso}!</h3>
+     <h3>Caminhão ${token} está em estado de ${grauDeAviso}!</h3>
     <small>Temperatura ${temp}.</small>   
+    </div>
+    <div class="alarme-sino"></div>
+    </div>`;
+}
+  
+  function atualizacaoPeriodica() {
+    obterdados2(2000)
+    
+    obterdados3(2000)// obterdados2(2001)
+    setTimeout(atualizacaoPeriodica, 5000);
+    }
+
+
+
+    var alertas2 = [];
+
+  
+function obterdados3(token){
+fetch(`/medidas/ultimas/${token}`)
+.then(resposta => {
+    if (resposta.ok) {
+        resposta.json().then(resposta =>{
+            console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
+            alertar2(resposta, token);
+        }); 
+    } else {
+      console.error('Nenhum dado encontrado ou erro na API');
+    }
+  })
+    .catch(function (error) {
+      console.error(`Erro na obtenção dos dados p/ gráfico: ${error.message}`);
+    });
+
+}
+
+function alertar2(resposta, token) {
+    var umi = resposta[0].umidade;
+
+    console.log(token === resposta[0].fkSensor)
+    
+    var grauDeAviso ='';
+
+    var limites = {
+        muito_quente: 90,
+        quente:87,
+        ideal: 80,
+        frio: 76,
+        muito_frio: 75
+    };
+
+    var classe_umidade = 'cor-alerta';
+
+    if (umi >= limites.muito_quente) {
+        classe_umidade = 'cor-alerta perigo-umido';
+        grauDeAviso = 'perigo-umido'
+        grauDeAvisoCor = 'cor-alerta perigo-umido'
+        exibirAlerta2(umi, token, grauDeAviso, grauDeAvisoCor)
+    }
+    else if (umi < limites.muito_quente && umi >= limites.quente) {
+        classe_umidade = 'cor-alerta alerta-umido';
+        grauDeAviso = 'alerta-umido'
+        grauDeAvisoCor = 'cor-alerta alerta-umido'
+        exibirAlerta2(umi, token, grauDeAviso, grauDeAvisoCor)
+    }
+    else if (umi < limites.quente && umi > limites.frio) {
+        classe_umidade = 'cor-alerta ideal2';
+        removerAlerta2(token);
+    }
+    else if (umi <= limites.frio && umi > limites.muito_frio) {
+        classe_umidade = 'cor-alerta alerta-seco';
+        grauDeAviso = 'alerta-seco'
+        grauDeAvisoCor = 'cor-alerta alerta-seco'
+        exibirAlerta2(umi, token, grauDeAviso, grauDeAvisoCor)
+    }
+    else if (umi <= limites.muito_frio) {
+        classe_umidade = 'cor-alerta perigo-seco';
+        grauDeAviso = 'perigo-seco'
+        grauDeAvisoCor = 'cor-alerta perigo-seco'
+        exibirAlerta2(umi, token, grauDeAviso, grauDeAvisoCor)
+    }
+
+    var card;
+
+    card_umi.innerHTML = umi + "%";
+    // card = document.getElementById("");
+    // card.className = classe_umidade;
+
+    // if (token == 1) {
+    //     temp_aquario_1.innerHTML = temp + "°C";
+    //     card = card_1
+    // } else if (token == 2) {
+    //     temp_aquario_2.innerHTML = temp + "°C";
+    //     card = card_2
+    // } else if (token == 3) {
+    //     temp_aquario_3.innerHTML = temp + "°C";
+    //     card = card_3
+    // } else if (token == 4) {
+    //     temp_aquario_4.innerHTML = temp + "°C";
+    //     card = card_4
+    // }
+
+
+}
+
+function exibirAlerta2(umi, token, grauDeAviso, grauDeAvisoCor) {
+    var indice = alertas2.findIndex(item => item.token == token);
+
+    if (indice >= 0) {
+        alertas2[indice] = { token, umi, grauDeAviso, grauDeAvisoCor }
+    } else {
+        alertas2.push({ token, umi, grauDeAviso, grauDeAvisoCor });
+    }
+
+    exibirCards2();
+    
+// Dentro da div com classe grauDeAvisoCor há um caractere "invisível", 
+// que pode ser inserido clicando com o seu teclado em alt+255 ou pelo código adicionado acima.
+}
+
+function removerAlerta2(token) {
+    alertas2 = alertas2.filter(item => item.token != token);
+    exibirCards2();
+}
+ 
+function exibirCards2() {
+    alerta2.innerHTML = '';
+
+    for (var i = 0; i < alertas2.length; i++) {
+        var mensagem = alertas2[i];
+        alerta2.innerHTML += transformarEmDiv2(mensagem);
+    }
+}
+
+function transformarEmDiv2({ token, umi, grauDeAviso, grauDeAvisoCor }) {
+    return `<div class="mensagem-alarme">
+    <div class="informacao">
+    <div class="${grauDeAvisoCor}">&#12644;</div> 
+     <h3>Caminhão ${token} está em estado de ${grauDeAviso}!</h3>
+    <small>Umidade ${umi}.</small>   
     </div>
     <div class="alarme-sino"></div>
     </div>`;
