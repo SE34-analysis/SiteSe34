17b9ac3abe4bce4137e32de240370bdbd039fa51
kaori_katayama <michelly.katayama@sptech.school>
2023-06-04 23:23:47 -0300

    Beta Site Antigo

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -20,15 +20,10 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH
 var mySqlConfig = {
     host: "localhost",
-<<<<<<< HEAD
     database: "SE34_analysis1",
     user: "root",
     password: "@Icecubes123",
-=======
-    database: "SE34_analysis",
-    user: "root",
-    password: "181004Mp.",
->>>>>>> 928cb8ccb85d8863e7821580f1f08df67571e1e4
+
 };
 
 function executar(instrucao) {
